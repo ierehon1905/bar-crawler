5a82e4854175e9274d0a272b275ff384d0d2cb12
Leon Minasyan <leon-minasyan@yandex-team.ru>
2022-09-21 01:06:30 +0300

    feat: yamaps

--- src/components/GoButton/GoButton.tsx
+++ src/components/GoButton/GoButton.tsx
@@ -1,57 +1,76 @@
 import { motion } from "framer-motion";
-import React from 'react';
-import './GoButton.css';
+import React from "react";
 
-import { BarInfo } from "../../entities/bar"
-import { getTaxiUrl } from "../../utils/taxi"
+import { BarInfo, getBarDistance } from "../../entities/bar";
+import { getTaxiUrl } from "../../utils/taxi";
+import { getMapsUrl } from "../../utils/yamaps";
+
+import "./GoButton.css";
 
 type Props = {
-    bar: BarInfo;
-    position?: GeolocationPosition;
-    selectCurrentBar: () => void;
-    tryIncrementCount: () => void;
-    textStyle: any;
-    isRandoming: boolean;
-}
+  bar: BarInfo;
+  position?: GeolocationPosition;
+  selectCurrentBar: () => void;
+  tryIncrementCount: () => void;
+  textStyle: any;
+  isRandoming: boolean;
+};
 
 export function GoButton(p: Props) {
-    const barUrl = getTaxiUrl({
-        endLat: p.bar.coords[0],
-        endLon: p.bar.coords[1],
-        startLat: p.position?.coords.latitude,
-        startLon:p.position?.coords.longitude,
+  // Кажется эта штука считаться будет каждый раз когда анимация меняет бар
+  // Надо бы избежать этого
+  let barUrl: string;
+  const distance =
+    p.position === undefined
+      ? undefined
+      : getBarDistance(
+          [p.position.coords.latitude, p.position.coords.longitude],
+          p.bar
+        );
+
+  if (distance && distance.distance < 50) {
+    barUrl = getMapsUrl({
+      endLat: p.bar.coords[0],
+      endLon: p.bar.coords[1],
+      startLat: p.position!.coords.latitude,
+      startLon: p.position!.coords.longitude,
     });
+  } else {
+    barUrl = getTaxiUrl({
+      endLat: p.bar.coords[0],
+      endLon: p.bar.coords[1],
+      startLat: p.position?.coords.latitude,
+      startLon: p.position?.coords.longitude,
+    });
+  }
 
+  function onClick() {
+    p.selectCurrentBar();
+    p.tryIncrementCount();
+  }
 
-    function onClick() {
-        p.selectCurrentBar();
-        p.tryIncrementCount();
-    }
-
-    return (
-      <motion.div 
-        className="go-btn blinking" 
+  return (
+    <motion.div
+      className="go-btn blinking"
+      style={{
+        pointerEvents: p.isRandoming ? "none" : "all",
+        display: p.isRandoming ? "none" : "block",
+      }}
+    >
+      <a
         style={{
-          pointerEvents: p.isRandoming ? 'none' : 'all',
-          display: p.isRandoming ? 'none' : 'block',
+          display: "flex",
+          ...(p.textStyle || {}),
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "60px",
+          height: "60px",
         }}
+        href={barUrl}
+        onClick={onClick}
       >
-        <a
-          style={{
-              display: "flex",
-              ...(p.textStyle || {}),
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontSize: '60px',
-              height: '60px',
-          }}
-          href={barUrl}
-          onClick={onClick}
-        >
-          <span className="shadow-text">
-              GO
-          </span>
-        </a>
-      </motion.div>
-    )
+        <span className="shadow-text">GO</span>
+      </a>
+    </motion.div>
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import svgLoader from 'vite-svg-loader'
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgLoader({ svgo: false})]
-})
+  plugins: [react(), svgLoader({ svgo: false }), basicSsl()],
+});
